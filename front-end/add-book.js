5c15dd037bff388ce3c18a35e4d0b8fa7ee42304
Joscelyn James <josce.james7@hotmail.com>
2017-10-03 12:32:12 -0600

    user story is complete. had to add route to get one book in order to get the specific books information to autofill the form.

--- front-end/add-book.js
+++ front-end/add-book.js
@@ -1,21 +1,63 @@
 $(() => {
+  const dataBaseURL = 'http://localhost:3211';
+  const frontEndURL = 'http://localhost:3004';
+  //get href and the query attached
+  var url = window.location.href;
+  var id = url.substring(url.indexOf('?') + 4);
+console.log(id);
+  //hide and show of the success-message
   $('#success-message').hide()
+    $('#submit-book-bttn').click((e) => {
+      e.preventDefault();
+      $('#success-message').show()
+    })
 
-  $('#add-book-bttn').click((e) => {
-    e.preventDefault();
-    $('#success-message').show()
-  })
+    // //change button to say edit
+    // $('#submit-book-bttn').val('Edit Book')
+    // $('#submit-book-bttn').attr('id', 'edit-book-btn')
 
+    function getBook(id) {
+      return fetch(`${dataBaseURL}/book?id=${id}`)
+        .then(res => res.json())
+        .then(res => {
+          // extract need data from res
+          let title = res[0].title;
+          let author = res[0].author;
+          let genre = res[0].genre;
+          let description = res[0].description;
+          let cover_img = res[0].cover_img;
 
-  $('.form-floating-label input, .form-floating-label textarea').focusin(function() {
-    $(this).parent().addClass('has-value');
-  });
+          // auto populate the form with the data above
+          $('#title').val(title);
+          $('#author').val(author);
+          $('#genre').val(genre);
+          $('#description').val(description);
+          $('#cover-img').val(cover_img);
+        })
+    };
 
-  $('.form-floating-label input, .form-floating-label textarea').blur(function() {
-    if (!$(this).val().length > 0) {
-      $(this).parent().removeClass('has-value');
-    }
-  });
+    getBook(id);
 
+    //click handler to update book info
+    $('#submit-book-bttn').click((e) => {
+      e.preventDefault();
+
+      let data = {
+        id: id,
+        title: $('#title').val(),
+        author: $('#author').val(),
+        genre: $('#genre').val(),
+        description: $('#description').val(),
+        cover_img: $('#cover-img').val()
+      };
+      JSON.stringify(data)
+      console.log(data);
+
+      $.ajax({
+        url: `${dataBaseURL}/edit-book?id=${id}`,
+        type: 'PATCH',
+        data: data
+      }).catch((err) => console.log(err))
+    })
 
 })
