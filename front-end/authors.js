7382fc68fad8bcef80556f64be206eeb7f708fa2
Joscelyn James <josce.james7@hotmail.com>
2017-10-03 15:09:53 -0600

    d

---
+++ front-end/authors.js
@@ -0,0 +1,23 @@
+$(() => {
+  // urls are only for devlopment
+  const dataBaseURL = 'http://localhost:3211';
+  const frontEndURL = 'http://localhost:3004';
+  var url = window.location.href;
+  var id = url.substring(url.indexOf('?') + 4);
+
+  getAllAuthorsFromDatabase()
+  
+  function getAllAuthorsFromDatabase() {
+    return fetch(`${dataBaseURL}/authors`)
+      .then(res => res.json())
+      .then(res => {
+        let source = $('#all-authors-template').html();
+        let template = Handlebars.compile(source);
+        let context = {
+          author: res
+        };
+        let html = template(context);
+        $('#template').append(html);
+      })
+  };
+})
