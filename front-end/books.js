211f9263eefefb719db8b5d04c47ecc2e4c02993
Joscelyn James <josce.james7@hotmail.com>
2017-10-03 10:25:05 -0600

    finished user story 03. Added a delte button on each book that deletes from databse

--- front-end/books.js
+++ front-end/books.js
@@ -1,8 +1,37 @@
 $(() => {
-  const url = 'http://localhost:3211/';
+  //SetTimeout due to handlebars loading in after jQuery is tyring to apply the click handler
+  window.setTimeout(() => {
+    console.log('timeout ready');
+    $('#delete-bttn').click((e) => {
+      e.preventDefault();
+      id = e.target.className;
+      deleteBookFromDatabase(id);
+
+    });
+  }, 1000)
+
+  const url = 'http://localhost:3211';
+
+  getAllBooksFromDatabase();
+
+  function deleteBookFromDatabase(id) {
+    if (confirm('are you sure?')) {
+      $.ajax({
+        url: `${url}/delete-book?id=${id}`,
+        type: 'DELETE'
+      });
+      location.reload();
+    } else {
+      console.log('not delted');
+    }
+
+    // return fetch(`${url}/delete-book?id=${id}`)
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err, 'deleteBookFromDatabase function'))
+  }
 
   function getAllBooksFromDatabase() {
-    return fetch(url + 'books')
+    return fetch(`${url}/books`)
       .then(res => res.json())
       .then(res => {
         let source = $('#all-books-template').html();
@@ -14,6 +43,6 @@ $(() => {
         $('#template').append(html);
       })
   };
-  getAllBooksFromDatabase();
+
 
 })
