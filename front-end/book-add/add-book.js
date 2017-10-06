2da7f5cd3dbf62842e5f3a17d945c94ac768b255
Joscelyn James <josce.james7@hotmail.com>
2017-10-06 10:33:03 -0600

    finsihed with add book and edit book

--- front-end/book-add/add-book.js
+++ front-end/book-add/add-book.js
@@ -1,21 +1,20 @@
 $(() => {
   // urls are only for devlopment
-  const dataBaseURL = 'http://localhost:3211';
-  const  frontEndURL = 'http://groovy-cloud.surge.sh';
+  const dataBaseURL = 'https://fathomless-springs-90574.herokuapp.com';
+  
   var url = window.location.href;
   var id = url.substring(url.indexOf('?') + 4);
   let data;
 
 // hide the success-message
-  $('#success-message').hide()
-
+  $('#success-message').hide();
 
 // if the url contains a query for id and if it does you know you are editing a book
   if (url.indexOf('?id=') > -1) {
     getBook(id);
     $('#submit-book-bttn').val('Edit Book')
     $('#submit-book-bttn').attr('id', 'edit-book-bttn')
-  }
+  };
 
     function getBook(id) {
       return fetch(`${dataBaseURL}/book?id=${id}`)
@@ -31,7 +30,6 @@ $(() => {
         })
     };
 
-
     //click handler to update book info
     $('#edit-book-bttn').click((e) => {
       e.preventDefault();
@@ -43,16 +41,14 @@ $(() => {
         description: $('#description').val(),
         cover_img: $('#cover-img').val()
       };
-
-      $.ajax({
+     $.ajax({
         url: `${dataBaseURL}/edit-book?id=${id}`,
         type: 'PATCH',
         data: data
-      })
-      .then(() => {
-          $('#success-message').show()
-        })
-    })
+      }).then(() => {
+          $('#success-message').show();
+        });
+    });
 
 // if the query does not contain a query you know you need are adding a book
     $('#submit-book-bttn').click((e) => {
@@ -68,8 +64,9 @@ $(() => {
         url: `${dataBaseURL}/add-book`,
         type: 'POST',
         data: data
-      })
-      $('#success-message').show()
-    })
+      }).then(() => { 
+        $('#success-message').show();
+       })
+    });
 
-})
+});
